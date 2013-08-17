897f7a625719a0386fe390b55e903e553eb016d3
Cosimo Ranieri <co.ranieri@gmail.com>
2013-08-17 14:39:54 +0200

    first commit

---
+++ app/controllers/poker_controller.rb
@@ -0,0 +1,14 @@
+class PokerController < ApplicationController
+  def index
+    @poker = ::PokerHands.new
+
+    h1 = Deck::Hand.new([Deck::Card.new("2", "C"), Deck::Card.new("1", "H"), Deck::Card.new("7", "C"), Deck::Card.new("5", "S"), Deck::Card.new("3", "C")])
+    h2 = Deck::Hand.new([Deck::Card.new("5", "C"), Deck::Card.new("1", "C"), Deck::Card.new("6", "H"), Deck::Card.new("5", "C"), Deck::Card.new("5", "C")])
+    h3 = Deck::Hand.new([Deck::Card.new("2", "C"), Deck::Card.new("5", "H"), Deck::Card.new("4", "C"), Deck::Card.new("5", "C"), Deck::Card.new("5", "C")])
+    h4 = Deck::Hand.new([Deck::Card.new("5", "H"), Deck::Card.new("5", "S"), Deck::Card.new("3", "C"), Deck::Card.new("5", "C"), Deck::Card.new("5", "C")])
+    @poker.hands = [h1, h2, h3, h4]
+    @winner = @poker.winner
+
+  end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PokerTest::Application.config.secret_token = 'e98e29441ba1db76d62250c83058be1f8fbef92734614e81f2fb01e5dd02fd59a4b814ebab8b9722082042d773b24f595aea9c4883ec34d271c6edd12d331cb2'
